8df150366b64133644f0ab0adc9e9a0eaedce429
Declan Andrew <anysweetimes@naver.com>
2021-02-10 13:10:59 +0900

    청크 적용

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import styles from './index.css';
 function component()
 {
     const element = document.createElement('div');
-    element.innerHTML = "Hello Webpack";
+    element.innerHTML = "Hello Webpack1";
 
     console.log(styles);
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,30 @@ const path = require('path');
 */ 
 // use배열에 문자열만 넣으면 loader 이름만 들어간거임(나머지 생략)
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports =
 {
     entry: "./src/index.js",
     output: {
-        path:path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path:path.resolve(__dirname, 'dist'), 
+        filename: 'bundle.[hash].js' // hash, contentHash chunkHash
     },
     mode:"none",
     module:{
         rules:[{
             test:/\.css$/i,
             use:[
+                // {
+                //     loader:'style-loader',
+                //     options:{
+                //         injectType:'singletonStyleTag'
+                //     }
+                // }, 외부 빠지는거 때문에 여기 위치
                 {
-                    loader:'style-loader',
-                    options:{
-                        injectType:'singletonStyleTag'
-                    }
+                    loader:MiniCssExtractPlugin.loader
                 },
                 {
                     loader:'css-loader',
@@ -29,7 +36,23 @@ module.exports =
                     }
                 }
             ]
+        },{
+            test:/\.hbs$/,
+            use:['handlebars-loader']
         }]
     },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename:'[contenthash].css' // 자바스크립트와 css 빌드 시점이 다르므로 contenthash 적용
+        }), // CSS 외부로 빠짐 CSS도 캐싱제외가능
+        new HtmlWebpackPlugin({
+            title:'Webpack',
+            template:'./template.hbs',
+            meta:{
+                viewport:"width=device-width, initial-scale=1.0"
+            }
+        }),
+        new CleanWebpackPlugin()
+    ],
     target:'web'
 }
